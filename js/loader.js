50ea445de41a4ccf9c5c6b3f295af8f3c3dfe7f5
Rabindra Tamang <rabee.tmg123@gmail.com>
2021-08-16 15:43:36 +0545

    Website of easestartup

---
+++ js/loader.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('body').css('display', 'none');
+    $('body').fadeIn(1000);  
+    $('.modal').modal();
+    // $('.parallax').parallax();
+    // // $('.slider').slider({full_width:true});
+    // $('.tabs').tabs();
+    $('.blank-section').delay(1000).css('display', 'block');
+    $('.hero').css('display', 'none');
+    $('.hero').fadeIn(1000);
+    $('.hero').css('display', 'none');
+    $('.hero').slideDown(700);  
+
+})
+
+function toggleModal(){
+    var instance=M.Modal.getInstance($("#modal3"));
+    instance.open();
+}
